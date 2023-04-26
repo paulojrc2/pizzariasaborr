046553fbc1934b490d4e24ce9a8fcc33e94623b3
Paulo Júnior <paulojrcosta300@gmail.com>
2023-04-26 11:43:48 -0300

    Creating pizza orders

--- process/pizza.php
+++ process/pizza.php
@@ -24,13 +24,49 @@
     $massa = $data["massa"];
     $sabores = $data["sabores"];
 
-    // Validação de sabores máximos
+    // Validação de valores máximo dos sabores 
     if(count($sabores) > 3) {
       $_SESSION["msg"] = "Selecione no máximo 3 sabores!";
       $_SESSION["status"] = "warning";
     } else {
-      echo "Passou da validação"; 
-      exit;
+      // Salvando borda e massa na pizza
+      $stmt = $conn->prepare("INSERT INTO pizzas (borda_id, massa_id) VALUES (:borda, :massa)");
+
+      // Filtrando inputs
+      $stmt->bindParam(":borda", $borda, PDO::PARAM_INT);
+      $stmt->bindParam(":massa", $massa, PDO::PARAM_INT);
+
+      $stmt->execute();
+
+      // Resgatando id da última pizza
+      $pizzaId = $conn->lastInsertId();
+
+      $stmt = $conn->prepare("INSERT INTO pizza_sabor (pizza_id, sabor_id) VALUES (:pizza, :sabor)");
+
+      // Repetição até salvar todos sabores
+      foreach($sabores as $sabor) {
+        // Filtrando inputs 
+        $stmt->bindParam(":pizza", $pizzaId, PDO::PARAM_INT);
+        $stmt->bindParam(":sabor", $sabor, PDO::PARAM_INT);
+
+        $stmt->execute();
+      }
+
+      // Criando pedido da pizza
+      $stmt = $conn->prepare("INSERT INTO pedidos (pizza_id, status_id) VALUES (:pizza, :status)");
+
+      // Status inicando com valor default "1", que é em produção
+      $statusId = 1;
+
+      // Filtrando inputs
+      $stmt->bindParam(":pizza", $pizzaId);
+      $stmt->bindParam(":status", $statusId);
+
+      $stmt->execute();
+
+      // Exibir mensagem de sucesso
+      $_SESSION["msg"] = "Pedido realizado com sucesso";
+      $_SESSION["status"] = "success";
     }
 
     // Retorna para página inicial
